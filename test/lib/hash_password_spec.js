8716524e348f1958bb581377de5a4df6a9d434ad
Erik Sälgström Peterson <eriksalgstrom@gmail.com>
2017-10-16 16:04:16 -0400

    Add hash password function to lib

---
+++ test/lib/hash_password_spec.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const hashPassword = require('../../lib/hash_password');
+const expect = require('expect.js');
+const bcrypt = require('bcrypt');
+
+describe('hashPassword(password)', () => {
+	it('returns the password, hashed', async () => {
+		let hashedPassword = await hashPassword('123456');
+		let compared = await bcrypt.compare('123456', hashedPassword);
+		expect(compared).to.be.ok();
+	});
+});
