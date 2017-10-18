69f982a9277b52c7ceca6bae0878cbe3a725a3df
Erik Sälgström Peterson <eriksalgstrom@gmail.com>
2017-10-18 01:37:27 -0400

    Fix coverage

--- test/lib/get_token_spec.js
+++ test/lib/get_token_spec.js
@@ -1,11 +1,30 @@
 'use strict';
 
 const expect = require('expect.js');
+const crypto = require('crypto');
 const getToken = require('../../lib/get_token');
+const sinon = require('sinon');
 
 describe('async getToken()', () => {
 	it('returns a token', async () => {
 		let token = await getToken();
 		expect(token).to.be.ok();
-	})
+	});
+
+	it('raises any crypto errors', async () => {
+		let stub = sinon.stub(crypto, 'randomBytes').callsFake((b, cb) => {
+			return cb(new Error('err'), '');
+		});
+
+		try{
+			await getToken();
+			expect().fail();
+		} catch(e){
+			console.error(e);
+			expect(e.message).to.be('err');
+		}
+		stub.restore();
+		
+	});
+
 });
