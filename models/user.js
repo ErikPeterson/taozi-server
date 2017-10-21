c159eb82a2233042b91bc71affdcc2b273c5296f
Erik Sälgström Peterson <eriksalgstrom@gmail.com>
2017-10-21 12:36:34 -0400

    Fix username regex

--- models/user.js
+++ models/user.js
@@ -9,7 +9,7 @@
 */
 
 const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-const NAME_REGEX = /[^aA-zZ_]/
+const NAME_REGEX = /[^0-9aA-zZ_]/
 const BaseModel = require('./base');
 const hashPassword = require('../lib/hash_password');
 const comparePassword = require('../lib/compare_password');
