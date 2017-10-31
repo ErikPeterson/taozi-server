5be3a9b09542bda4bd06adc8d55b3b9776398162
Erik Sälgström Peterson <eriksalgstrom@gmail.com>
2017-10-30 22:50:19 -0400

    Spec coverage

--- test/models/errors/record_not_found_spec.js
+++ test/models/errors/record_not_found_spec.js
@@ -8,6 +8,7 @@ describe('RecordNotFound', () => {
 		it('returns a new instance of RecordNotFound', () => {
 			let err = new RecordNotFound('FakeModel', {_id: 1234});
 			expect(err.message).to.match(/could not find FakeModel with query={"_id":1234}/)
+			expect(err.status).to.be(404);
 		});
 	});
 });
