8a81fe41a45b8710deec29a05c2e8b59788b1639
Erik Sälgström Peterson <eriksalgstrom@gmail.com>
2017-11-11 00:00:05 -0500

    Add cors for local development

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 'use strict';
 
+const NODE_ENV = process.env.NODE_ENV || 'development';
 const statuses = require('statuses');
 statuses[420] = 'Increase your chill';
 
 const Koa = require('koa');
+const cors = require('koa2-cors');
 const app = new Koa();
 
 const {logger, middleware} = require('./lib/logger')();
@@ -12,6 +14,14 @@ const catchErrors = require('./lib/catch_errors')(logger);
 app.use(middleware);
 
 app.use(catchErrors);
+
+if(NODE_ENV !== 'production') app.use(cors({
+	maxAge: 7 * 24 * 60 * 60, 
+	origin: '*',
+	authentication: true,
+	allowMethods: ['GET', 'POST', 'OPTIONS', 'DELETE'], 
+	allowHeaders: ['Content-Type', 'Authorization', 'Accept']
+}));
 
 const router = require('./routes/router.js')(logger);
 app.use(router.routes());
